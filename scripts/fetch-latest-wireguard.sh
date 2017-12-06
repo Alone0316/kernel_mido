f9bee6dd3d762bd74dbe8bd8814b57266e5fa90f
Jason A. Donenfeld <Jason@zx2c4.com>
2017-12-06 21:35:22 +0100

    net: wireguard: Add tools to import source automatically
    ./scripts/fetch-latest-wireguard.sh
    
    -drivers/net/wireguard
    
    -wg: Include wireguard source  --- It's already included 35ad43142366b748c65485a849f65eaafcdb68e8
    
    Conflicts:
           net/Kconfig
           net/Makefile
    
    Link: https://github.com/AICP/kernel_google_wahoo/commit/3f383b75e62e1b3073243f27ac54f2bb837f73a1
    Signed-off-by: Jason A. Donenfeld" <Jason@zx2c4.com>
    Change-Id: Ief23eeef20744788beacf1a27357593d8c710e03
    Signed-off-by: TogoFire <italomellopereira@gmail.com>

---
+++ scripts/fetch-latest-wireguard.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+USER_AGENT="WireGuard-AndroidROMBuild/0.3 ($(uname -a))"
+
+while read -r distro package version _; do
+	if [[ $distro == upstream && $package == linuxcompat ]]; then
+		VERSION="$version"
+		break
+	fi
+done < <(curl -A "$USER_AGENT" -LSs --connect-timeout 30 https://build.wireguard.com/distros.txt)
+
+[[ -n $VERSION ]]
+
+if [[ -f drivers/net/wireguard/version.h && $(< drivers/net/wireguard/version.h) == *$VERSION* ]]; then
+	touch drivers/net/wireguard/.check
+	exit 0
+fi
+
+rm -rf drivers/net/wireguard
+mkdir -p drivers/net/wireguard
+curl -A "$USER_AGENT" -LsS --connect-timeout 30 "https://git.zx2c4.com/wireguard-linux-compat/snapshot/wireguard-linux-compat-$VERSION.tar.xz" | tar -C "drivers/net/wireguard" -xJf - --strip-components=2 "wireguard-linux-compat-$VERSION/src"
+sed -i 's/tristate/bool/;s/default m/default y/;' drivers/net/wireguard/Kconfig
+touch drivers/net/wireguard/.check
