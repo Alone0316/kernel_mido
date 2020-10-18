85afb2f47d5838af7aa395e58a51a42de1901de9
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-10-18 12:58:07 +0530

    drm: atomic/sde_crtc: Do not boost CPU/DDR after input timeout
     * this prevents cpu or ddr bus from boosting unnecessarily during video playback
       and similar usecases where a CPU boost is unnecessary during a frame commit
    
     * the default timeout of 3250 ms has been empirically determined by kdrag0n by
       performing multiple flings at various speeds while timing them. he observed
       the maximum fling time to be approximately 2850 ms, so our frame boost timeout
       of 3250 ms offers some headroom for more intensive applications such as games.

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -1905,11 +1905,9 @@ int drm_mode_atomic_ioctl(struct drm_device *dev,
 			(arg->flags & DRM_MODE_PAGE_FLIP_EVENT))
 		return -EINVAL;
 
-	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
-		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
-		cpu_input_boost_kick();
-
-	}
+	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY))
+		if (df_boost_within_input(3250))
+			devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
 
 	drm_modeset_acquire_init(&ctx, 0);
 
--- drivers/gpu/drm/msm/sde/sde_crtc.c
+++ drivers/gpu/drm/msm/sde/sde_crtc.c
@@ -3755,6 +3755,9 @@ void sde_crtc_commit_kickoff(struct drm_crtc *crtc,
 
 	SDE_ATRACE_BEGIN("crtc_commit");
 
+	if (cpu_input_boost_within_input(3250))
+		cpu_input_boost_kick();
+
 	is_error = _sde_crtc_prepare_for_kickoff_rot(dev, crtc);
 
 	idle_pc_state = sde_crtc_get_property(cstate, CRTC_PROP_IDLE_PC_STATE);
