16b63803bce162597e9f219d8f2c408aac88012d
Emanuel <emanuelghub@gmail.com>
2021-05-30 20:45:03 +0400

    lpm-levels: Don't expose sleep_disabled * Don't allow userspace do anything with it

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -83,9 +83,6 @@ static void cluster_prepare(struct lpm_cluster *cluster,
 static bool print_parsed_dt;
 module_param_named(print_parsed_dt, print_parsed_dt, bool, 0664);
 
-static bool sleep_disabled;
-module_param_named(sleep_disabled, sleep_disabled, bool, 0664);
-
 /**
  * msm_cpuidle_get_deep_idle_latency - Get deep idle latency value
  *
