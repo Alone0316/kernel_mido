56483e4f9b07b1d0002493698b8165d3acde758b
zeta96 <ahmad.zaainul857@gmail.com>
2023-03-18 21:14:25 +0000

    PM / devfreq: Make min_freq node read only

--- drivers/devfreq/devfreq.c
+++ drivers/devfreq/devfreq.c
@@ -1107,7 +1107,7 @@ static ssize_t polling_interval_store(struct device *dev,
 }
 static DEVICE_ATTR_RW(polling_interval);
 
-static ssize_t min_freq_store(struct device *dev, struct device_attribute *attr,
+static ssize_t  __maybe_unused min_freq_store(struct device *dev, struct device_attribute *attr,
 			      const char *buf, size_t count)
 {
 	struct devfreq *df = to_devfreq(dev);
@@ -1178,7 +1178,7 @@ static ssize_t name##_show					\
 show_one(min_freq);
 show_one(max_freq);
 
-static DEVICE_ATTR_RW(min_freq);
+static DEVICE_ATTR_RO(min_freq);
 static DEVICE_ATTR_RO(max_freq);
 
 static ssize_t available_frequencies_show(struct device *d,
